a7d393c062f9759bf5f9443db7b5d9efc3361c13
Chris Samuel <ksamuel.chris@gmail.com>
2018-04-18 19:21:49 -0400

    added two examples for using arrow function and why

--- application.js
+++ application.js
@@ -6,8 +6,63 @@ var createGreeting = function(message, name) {
 
 // In the arrow function expression you don't use the return keyword
 // In the arrow function expression you do not have to use the braces.
-var arrowGreeting = (message ,  name ) => message + name;
+//multi-variable
+var arrowGreeting = (message,name ) => message + name;
 
+// single variable
+var arrowSingleVarGreeting = message => "Single Variable"
+
+/**
+ *   var oldES5Greeting = function(message) {
+ *   return message;
+ * }
+ * 
+ */
+
+var squared = x => x * x ;
 
 // console.log(createGreeting("Hello ", "Chris"));
 console.log(arrowGreeting("Hello ", "Chris"));
+
+console.log(arrowSingleVarGreeting("Single Variable"));
+
+console.log(squared(5));
+
+
+var deliveryBoy ={
+    name: "Old Boy",
+    
+    handleMessage: function (message,handler){
+    handler(message);
+    },
+    
+    receive: function () {
+      this.handleMessage("Hello, ", message => console.log(message + this.name))
+    }
+  }
+  
+  deliveryBoy.receive();
+  
+
+      var arrowFunctionDeliveryBoy = {
+        name: "Chris",
+        
+        handleMessage: function (message,handler){
+        handler(message);
+        },
+        
+        receive: function () {
+            var that = this;  // get proper name
+            
+            this.handleMessage("Hello, ", (message) => {
+                     that.name ;
+                     console.log(message + that.name);
+            })
+        }
+        
+      }
+      
+      arrowFunctionDeliveryBoy.receive();
+      
+    
+  
