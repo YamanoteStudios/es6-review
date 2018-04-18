fa554b70d2d1e4287da676531ccff65f91d2c6fd
Chris Samuel <ksamuel.chris@gmail.com>
2018-04-18 18:03:21 -0400

    used a arrow function method

--- application.js
+++ application.js
@@ -4,4 +4,10 @@ var createGreeting = function(message, name) {
 }
 
 
-console.log(createGreeting("Hello ", "Chris"));
+// In the arrow function expression you don't use the return keyword
+// In the arrow function expression you do not have to use the braces.
+var arrowGreeting = (message ,  name ) => message + name;
+
+
+// console.log(createGreeting("Hello ", "Chris"));
+console.log(arrowGreeting("Hello ", "Chris"));
