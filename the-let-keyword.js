44a13284baa25728ec1f121a3789967da541cd75
Chris Samuel <ksamuel.chris@gmail.com>
2018-04-18 21:54:47 -0400

    using the let example in for loop  instead of var

--- the-let-keyword.js
+++ the-let-keyword.js
@@ -15,3 +15,43 @@ function greet() {var message =  "bye"}
 }
 
 console.log(message); // bye
+
+ 
+  
+  
+
+  // display in plunker preview
+  function displayInPreview(string) {
+    var newDiv = document.createElement("div"); 
+    var newContent = document.createTextNode(string); 
+    newDiv.appendChild(newContent);
+    document.body.appendChild(newDiv)
+  }
+
+
+  var fs = [];
+
+/* using let in the for loop generates a new i for each iteration  */
+  for(let i = 0 ; i < 10; i++) {
+      fs.push(function () {
+          console.log(i); // this sign is the same i each time
+      })
+  }
+
+  fs.forEach(function(f) {
+      f();
+      /**
+       * Bunch of 10s
+       *10
+        10
+        10
+        10
+        10
+        10
+        10
+        10
+        10
+        10 */
+  })
+
+  
